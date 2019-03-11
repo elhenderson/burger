2e9c88358c9e1a3628c6a9aeafd86e259284c2e6
elhenderson <evann.henderson2@yahoo.com>
2019-03-11 18:11:23 -0500

    set up

---
+++ db/seeds.sql
@@ -0,0 +1,5 @@
+INSERT INTO `burgers_db`.`burgers`
+(`id`,
+`burger_name`,
+`devoured`)
+VALUES (null, "Double Cheeseburger", false), (null, "Patty Melt", true), (null, "Barbeque Bacon Burger", false)

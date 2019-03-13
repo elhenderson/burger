6e58671809bbd62cab441baca231cdaa75aa6d04
elhenderson <evann.henderson2@yahoo.com>
2019-03-13 18:15:05 -0500

    heroku

--- config/connection.js
+++ config/connection.js
@@ -1,13 +1,19 @@
 var mysql = require('mysql');
 
-var connection = mysql.createConnection({
-    host: "localhost",
-    port: 3306,
-    user: "root",
-    password: "password",
-    database: "burgers_db"
-  })
+if (process.env.JAWSDB_URL) {
+    connection = mysql.createConnection(process.env.JAWSDB_URL)
+    } else {
+        var connection = mysql.createConnection({
+            host: "localhost",
+            port: 3306,
+            user: "root",
+            password: "password",
+            database: "burgers_db"
+        })
+    }
   
+
+
 connection.connect((err) => {
   if (err) {
       console.log(`Could not connect: ${err.stack}`)
